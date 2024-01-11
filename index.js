51ed5b95af01bbd27ceae269379a178773062603
Rodrigo Barrera <bar20807@uvg.edu.gt>
2024-01-10 22:58:49 -0600

    frontend-added

--- index.js
+++ index.js
@@ -71,6 +71,7 @@ app.use(express.json())
 app.use(requestLogger)
 app.use(morgan('tiny'))
 app.use(cors())
+app.use(express.static('build'))
 
 //Ahora se agregaran las notas que debera de devolver el listado de notas codificadas en formato JSON
 
